aa4c512fda46a1d7b4c7bc11c8650381a1eaa7ac
Walter Vincenti <vincentiwalter@gmail.com>
2026-05-16 12:44:12 +0200

    sheet_cells.sql restored

--- sql/read/sheet_cells.sql
+++ sql/read/sheet_cells.sql
@@ -1,3 +1,18 @@
-SELECT COLUMN_NAME, DATA_TYPE 
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_SCHEMA = 'cell_master' AND TABLE_NAME = 'row_connections';
+SELECT 
+	sheet.name
+	,col.name
+	,col.index
+	,cell.row_index
+	,cell.col_index
+	,cell.display_val
+
+FROM users_sheets u_s
+	INNER JOIN sheets sheet ON (sheet.id = u_s.sheet_id)
+	INNER JOIN cols col ON (col.index = sheet.col_index)
+	INNER JOIN cells cell ON (cell.col_index = col.index)
+WHERE 
+	cell.sheet_id = ? 
+	AND u_s.sheet_id = ?
+ORDER BY
+	cell.row_id
+	,cell.col_index
